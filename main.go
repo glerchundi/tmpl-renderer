683d29b017b6c5c7815ebd071be381ebea46d066
Gorka Lerchundi Osa <glertxundi@gmail.com>
2016-06-08 10:04:13 +0200

    feat: expose more template funcs
    - getEnv
    - getFileContent
    - getFileContentBytes
    - gzip
    - encodeBase64

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
 	"strings"
 	"text/template"
-	"path"
 
 	flag "github.com/spf13/pflag"
-	log "github.com/glerchundi/logrus"
-	"fmt"
 )
 
 const (
@@ -59,21 +62,52 @@ func main() {
 
 	// check if file exists
 	if _, err := os.Stat(tmplFile); os.IsNotExist(err) {
-		log.Fatal(err)
+		log.Fatalf("%s", err)
 	}
 
 	// template funcs
 	funcMap := make(map[string]interface{})
 	funcMap["getenv"] = os.Getenv
+	funcMap["getEnv"] = os.Getenv
+	funcMap["getFileContent"] = func(i string) string {
+		d, err := ioutil.ReadFile(i)
+		if err != nil {
+			return ""
+		}
+		return string(d)
+	}
+	funcMap["getFileContentBytes"] = func(i string) []byte {
+		d, err := ioutil.ReadFile(i)
+		if err != nil {
+			return nil
+		}
+		return d
+	}
+	funcMap["gzip"] = func(i []byte) []byte {
+		var b bytes.Buffer
+		w := gzip.NewWriter(&b)
+		if _, err := w.Write(i); err != nil {
+			return nil
+		}
+
+		if err := w.Close(); err != nil {
+			return nil
+		}
+
+		return b.Bytes()
+	}
+	funcMap["encodeBase64"] = func(i []byte) string {
+		return base64.StdEncoding.EncodeToString(i)
+	}
 
 	// create template
 	tmpl, err := template.New(path.Base(tmplFile)).Funcs(funcMap).ParseFiles(tmplFile)
 	if err != nil {
-		log.Fatal(fmt.Errorf("Unable to process template %s, %s", tmplFile, err))
+		log.Fatalf("Unable to process template %s, %s", tmplFile, err)
 	}
 
 	// and then execute ;)
-	if err = tmpl.Execute(os.Stderr, nil); err != nil {
-		log.Fatal(err)
+	if err = tmpl.Execute(os.Stdout, nil); err != nil {
+		log.Fatalf("%s", err)
 	}
 }
